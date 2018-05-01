46b9b24241667e9d61ad6eea7035fd4d1dedecd3
KaosMC <38406827+KaosMC@users.noreply.github.com>
2018-05-01 11:37:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-const Enmap = require("enmap");
+const Enmap = require("Enmap");
 const bot = new Discord.Client({
     disableEveryone: true
 });
