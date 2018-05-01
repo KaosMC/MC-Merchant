aa7315383bfc6c2b64abf75710b047f41be3407b
KaosMC <38406827+KaosMC@users.noreply.github.com>
2018-05-01 15:43:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ bot.on("message", async message => {
 
             if (user.roles.has(merchantRole.id)) {
                 if (amount < 3) {
-                    cooldowns.set(id, String.prototype.valueOf(Number(amount + 1)));
+                    cooldowns.set(id, (Number(amount + 1)).toString());
                 } else {
                     message.delete().then(() => {
                         let maxReached = "You have already reached your maximum amount of messages per day.";
